17cba6c5f53cf16b6fcd2ed3fd5b241eeabd150f
tknrng <51365939+tknrng@users.noreply.github.com>
2020-02-21 17:13:51 +0300

    Update string.rb

--- lib/ldap_sync/core_ext/string.rb
+++ lib/ldap_sync/core_ext/string.rb
@@ -20,7 +20,7 @@ require 'net/ldap'
 module Net::BER::Extensions::String
   if Gem.loaded_specs['net-ldap'].version < Gem::Version.new('0.12.0')
     def raw_utf8_encoded
-         if self.respond_to?(:encode)
+          if self.respond_to?(:encode)
       # Strings should be UTF-8 encoded according to LDAP.
       # However, the BER code is not necessarily valid UTF-8
       self.encode('UTF-8').force_encoding('ASCII-8BIT')
